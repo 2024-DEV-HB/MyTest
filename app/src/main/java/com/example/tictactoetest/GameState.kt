1660f0592ef8210c2db88deecbb8e227c7c56dfc
Bkoule <126973071+Bkoule@users.noreply.github.com>
2024-11-07 22:10:28 +0100

    Adds the playTurn method to handle turns and ignore invalid moves

--- app/src/main/java/com/example/tictactoetest/GameState.kt
+++ app/src/main/java/com/example/tictactoetest/GameState.kt
@@ -5,6 +5,13 @@ class GameState {
     val board = MutableList<Player?>(9) { null }
     var currentPlayer = Player.X
 
+    fun playTurn(position: Int) : Boolean {
+        if (position !in 0..8 || board[position] != null) return false
+        board[position] = currentPlayer
+        currentPlayer = if (currentPlayer == Player.X) Player.O else Player.X
+        return true
+    }
+
 }
 
 enum class Player {
