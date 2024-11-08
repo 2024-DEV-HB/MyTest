f8b8ce1f373b8543e871f2e3432f48b2cb7fa03a
Bkoule <126973071+Bkoule@users.noreply.github.com>
2024-11-08 03:45:01 +0100

    add a function to check if there is a winner

--- app/src/main/java/com/example/tictactoetest/GameState.kt
+++ app/src/main/java/com/example/tictactoetest/GameState.kt
@@ -12,6 +12,20 @@ class GameState {
         return true
     }
 
+    fun checkWinner(): Player? {
+        val winningPositions = listOf(
+            listOf(0, 1, 2), listOf(3, 4, 5), listOf(6, 7, 8), // Row
+            listOf(0, 3, 6), listOf(1, 4, 7), listOf(2, 5, 8), // Col
+            listOf(0, 4, 8), listOf(2, 4, 6), // Diagonal
+        )
+
+        winningPositions.forEach { (a, b, c) ->
+            if (board[a] != null && board[a] == board[b] && board[b] == board[c]) {
+                return board[a]
+            }
+        }
+        return null
+    }
 }
 
 enum class Player {
